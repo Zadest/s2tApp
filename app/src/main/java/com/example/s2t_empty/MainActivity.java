f69ec73e446b92ce9f95e47c47b9a2ec749c2c33
kbertho1 <k.bertholdt@web.de>
2020-12-16 09:25:13 +0100

    Audioplayer with example audio

--- app/src/main/java/com/example/s2t_empty/MainActivity.java
+++ app/src/main/java/com/example/s2t_empty/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.s2t_empty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private boolean State = false;
+    Button pause, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pause =(Button)findViewById(R.id.button2);
+        stop =(Button)findViewById(R.id.button3);
+        //'create': no need to prepare MediaPlayer
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.test);
+
+        //Clicklistener Play/Pause
+        pause.setOnClickListener(v -> {
+            if (mp.isPlaying()) {
+                mp.pause();
+            }else{
+                mp.start();
+            }
+        });
+
+        //Clicklistener Stop
+        stop.setOnClickListener(v -> {
+            mp.pause();
+            mp.seekTo(0);
+        });
     }
 
     public void changeText(View myView) {
