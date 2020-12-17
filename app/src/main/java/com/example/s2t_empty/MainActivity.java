6958db20068b9a571316546490b0fbb49daacadb
kbertho1 <k.bertholdt@web.de>
2020-12-17 19:33:53 +0100

    Buttons with ImageView

--- app/src/main/java/com/example/s2t_empty/MainActivity.java
+++ app/src/main/java/com/example/s2t_empty/MainActivity.java
@@ -2,56 +2,49 @@ package com.example.s2t_empty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private boolean State = false;
-    Button pause, stop;
+    //TODO: read in Whatsapp-Filenames (with special characters)
+    //TODO: foward/rewind?
+    //TODO: possible to read in shared file?
+    ImageView play_pause_icon;
+    ImageView stop_icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play_pause_icon = findViewById(R.id.play_pause);
+        stop_icon = findViewById(R.id.stop_play);
 
-        pause =(Button)findViewById(R.id.button2);
-        stop =(Button)findViewById(R.id.button3);
         //'create': no need to prepare MediaPlayer
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.test);
 
-        //Clicklistener Play/Pause
-        pause.setOnClickListener(v -> {
-            if (mp.isPlaying()) {
-                mp.pause();
-            }else{
+        //Click Listener for Playbutton
+        play_pause_icon.setOnClickListener(v -> {
+            if(!mp.isPlaying()){
                 mp.start();
+                play_pause_icon.setImageResource(R.drawable.ic_baseline_pause_24);
+            }else{
+                mp.pause();
+                play_pause_icon.setImageResource(R.drawable.ic_baseline_play_arrow_24);
             }
         });
 
-        //Clicklistener Stop
-        stop.setOnClickListener(v -> {
-            mp.pause();
-            mp.seekTo(0);
+        //Clicklistener for Stopbutton
+        stop_icon.setOnClickListener(v -> {
+                mp.pause();
+                mp.seekTo(0);
+                play_pause_icon.setImageResource(R.drawable.ic_baseline_play_arrow_24);
         });
     }
 
-    public void changeText(View myView) {
-        System.out.println("!Test");
-        TextView myText = (TextView) findViewById(R.id.textView5);
-        System.out.println(myText.getText());
-        // TODO : Request
-        if (State) {
-            myText.setText("Neuer Text");
-            State = !(State);
-        }
-        else{
-            myText.setText("Anderer Text");
-            State = !(State);
-        }
-    }
 
 }
