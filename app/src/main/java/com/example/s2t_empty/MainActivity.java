87c4ac3941bd391667befa7b6bc6a8a459c97480
tessajo <teresa-johnsen@t-online.de>
2021-01-19 20:07:37 +0100

    geschachtelte extends-methode, für Fragment Activity

--- app/src/main/java/com/example/s2t_empty/MainActivity.java
+++ app/src/main/java/com/example/s2t_empty/MainActivity.java
@@ -7,40 +7,47 @@ import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
+    public class MainActivity2 extends FragmentActivity {
 
-    private boolean State = false;
+        private boolean State = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            AppBarConfiguration appBarConfiguration =
+                    new AppBarConfiguration.Builder(navController.getGraph()).build();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            NavigationUI.setupWithNavController(
+                    toolbar, navController, appBarConfiguration);
+        }
+
+        public void changeText(View myView) {
+            System.out.println("!Test");
+            TextView myText = (TextView) findViewById(R.id.textView5);
+            System.out.println(myText.getText());
+            // TODO : Request
+            if (State) {
+                myText.setText("Neuer Text");
+                State = !(State);
+            } else {
+                myText.setText("Anderer Text");
+                State = !(State);
+            }
+        }
+
 
     }
-
-    public void changeText(View myView) {
-        System.out.println("!Test");
-        TextView myText = (TextView) findViewById(R.id.textView5);
-        System.out.println(myText.getText());
-        // TODO : Request
-        if (State) {
-            myText.setText("Neuer Text");
-            State = !(State);
-        }
-        else{
-            myText.setText("Anderer Text");
-            State = !(State);
-        }
-    }
-
-
-
 }
 
--- app/src/main/java/com/example/s2t_empty/SavedTexts.java
+++ app/src/main/java/com/example/s2t_empty/SavedTexts.java
@@ -5,25 +5,36 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
 public class SavedTexts extends AppCompatActivity {
+    public class SavedTexts2 extends FragmentActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-    }
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            AppBarConfiguration appBarConfiguration =
+                    new AppBarConfiguration.Builder(navController.getGraph()).build();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            NavigationUI.setupWithNavController(
+                    toolbar, navController, appBarConfiguration);
+        }
 
-    //@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_texts, container, false);
+        //@Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_saved_texts, container, false);
+        }
     }
 }
