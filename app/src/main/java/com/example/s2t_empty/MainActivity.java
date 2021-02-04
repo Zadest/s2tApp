50901eafb51969265e58fe05f894bf4e79a71ca5
tessajo <teresa-johnsen@t-online.de>
2021-02-04 15:04:33 +0100

    BottomNav erfolgreich implementiert

--- app/src/main/java/com/example/s2t_empty/MainActivity.java
+++ app/src/main/java/com/example/s2t_empty/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.s2t_empty;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                new StartScreen()).commit();
+    };
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            item -> {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.start_screen:
+                        selectedFragment = new StartScreen();
+                        break;
+                    case R.id.saved_text:
+                        selectedFragment = new SavedText();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                        selectedFragment).commit();
+                return true;
+            };
 }
