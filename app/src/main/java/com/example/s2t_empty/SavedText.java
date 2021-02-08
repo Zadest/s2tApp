e3f2366378c1b93bfac99b447f6af6a09cebd678
Moritz Eßer <info@horatiuslampenschirm.de>
2021-02-09 00:02:30 +0100

    OnClickListener Template in Fragments eingefuegt

--- app/src/main/java/com/example/s2t_empty/SavedText.java
+++ app/src/main/java/com/example/s2t_empty/SavedText.java
@@ -5,17 +5,29 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.view.ViewGroup;
 
-public class SavedText extends Fragment {
+public class SavedText extends Fragment implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_text, container, false);
+        View view =  inflater.inflate(R.layout.fragment_saved_text, container, false);
+
+        // Fuege hier neue Buttonlistener hinzu
+
+        return view;
     }
+
+
 }
